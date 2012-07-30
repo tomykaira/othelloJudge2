1b27e4838ebdcf2c495f94ca8335f9f8056b9a69
tomykaira <tomykaira@gmail.com>
2012-07-30 17:36:26 +0900

    Report error exit when server throws error

--- app/models/Server.scala
+++ app/models/Server.scala
@@ -1,6 +1,7 @@
 package models
 
 import java.io.{File, InputStreamReader, BufferedReader}
+import play.Logger
 
 /**
  * Server runner
@@ -18,29 +19,36 @@ extends Thread {
   override def run() = {
     val builder = new ProcessBuilder(SERVER_PROGRAM.toString,
       "-p", port.toString, "-t", ROUND_TIMEOUT.toString)
+    val stringBuilder = new StringBuilder()
     builder redirectErrorStream true
     builder directory SERVER_PROGRAM.getParentFile
 
-    val proc = builder.start
+    try {
+      val proc = builder.start
+      val streamReader = new InputStreamReader(proc.getInputStream)
+      val bufferedReader = new BufferedReader(streamReader)
+      var line:String = null
 
-    val streamReader = new InputStreamReader(proc.getInputStream)
-    val bufferedReader = new BufferedReader(streamReader)
-    val stringBuilder = new StringBuilder()
-    var line:String = null
-    while({line = bufferedReader.readLine; line != null}){
-      stringBuilder.append(line)
-      stringBuilder.append("\n")
+      while({line = bufferedReader.readLine; line != null}){
+        stringBuilder.append(line)
+        stringBuilder.append("\n")
+      }
+      bufferedReader.close
+
+      val result = stringBuilder.toString
+
+      proc.waitFor
+
+      if (proc.exitValue == 0) {
+        BattleRecorder.report(NormalExit(battle, result))
+      } else {
+        BattleRecorder.report(AbnormalExit(battle, result))
+      }
+    } catch {
+      case e: Exception =>
+        Logger.error("Othello server error", e)
+        BattleRecorder.report(AbnormalExit(battle, e.toString()))
     }
-    bufferedReader.close
 
-    val result = stringBuilder.toString
-
-    proc.waitFor
-
-    if (proc.exitValue == 0) {
-      BattleRecorder.report(NormalExit(battle, result))
-    } else {
-      BattleRecorder.report(AbnormalExit(battle, result))
-    }
   }
 }
