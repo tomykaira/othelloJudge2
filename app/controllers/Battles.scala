d36f91c1804dd1795dba16c5db014125b46f3d2f
tomykaira <tomykaira@gmail.com>
2012-08-10 09:57:21 +0900

    create pages for clients output

--- app/controllers/Battles.scala
+++ app/controllers/Battles.scala
@@ -12,20 +12,34 @@ import models.Battle
  */
 
 object Battles extends Controller {
+  def commonHeader (battle: Battle): List[String] = List(
+    "Black: " + battle.blackMail,
+    "Version: " + battle.blackVersion,
+    "White: " + battle.whiteMail,
+    "Version: " + battle.whiteVersion,
+    "Result: " + battle.status)
+
   def show (id: Long) = Action {
     Battle.findById(id).map { battle =>
-      val output = List(
-        "Black: " + battle.blackMail,
-        "Version: " + battle.blackVersion,
-        "White: " + battle.whiteMail,
-        "Version: " + battle.whiteVersion,
-        "Result: " + battle.status,
-        "Server output: " + battle.serverOutput,
-        "-------------------------------------",
-        "Black output: " + battle.blackOutput,
-        "-------------------------------------",
-        "White output: " + battle.whiteOutput
-      )
+      val output = commonHeader(battle) ++ List("Server output: " + battle.serverOutput)
+      Ok(output.mkString("\n")).as("text/plain")
+    }.getOrElse {
+      NotFound("There is no battle with id " + id).as("text/plain")
+    }
+  }
+
+  def clientBlack (id: Long) = Action {
+    Battle.findById(id).map { battle =>
+      val output = commonHeader(battle) ++ List("Black output: " + battle.blackOutput)
+      Ok(output.mkString("\n")).as("text/plain")
+    }.getOrElse {
+      NotFound("There is no battle with id " + id).as("text/plain")
+    }
+  }
+
+  def clientWhite (id: Long) = Action {
+    Battle.findById(id).map { battle =>
+      val output = commonHeader(battle) ++ List("White output: " + battle.whiteOutput)
       Ok(output.mkString("\n")).as("text/plain")
     }.getOrElse {
       NotFound("There is no battle with id " + id).as("text/plain")
